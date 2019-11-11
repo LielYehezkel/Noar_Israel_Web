2ceb5865e379070b2df44887c687989da64366c2
yovelsapir <yovelsapir@gmail.com>
2019-11-11 21:58:12 +0200

    implement loading comments

--- pages/dashboard/questionsAndComments.vue
+++ pages/dashboard/questionsAndComments.vue
@@ -5,7 +5,9 @@
     <v-container fluid>
       <v-row>
         <v-col v-for="(item, i) in categories" :key="i" cols="4">
-          <v-card class="ma-0 pa-3" outlined flat>
+          <v-card class="ma-0 pa-1" outlined flat>
+            <!-- :color="`rgba(${item.color[0]}, ${item.color[1]}, ${item.color[2]}, 0.9)`" -->
+            <!-- {{question.color}} -->
             <v-card-title>
               <div class="d-flex justify-space-between align-center" style="width: 100%;">
                 <span>{{item.name}}</span>
@@ -18,9 +20,9 @@
               </div>
             </v-card-title>
             <v-list flat style="max-height: 1000px;" class="overflow-y-auto">
-              <v-list-item-group v-for="(listItem, n) in listItems[i]" :key="n" multiple>
+              <v-list-item-group multiple>
                 <v-list-item
-                  v-for="(question, g) in listItem"
+                  v-for="(question, g) in listItems[i].data"
                   :key="g"
                   class="grey lighten-4 mt-2 mb-2"
                   :ripple="false"
@@ -36,12 +38,50 @@
                         <v-btn icon tile>
                           <v-icon>mdi-account-alert</v-icon>
                         </v-btn>
-                        <v-btn icon tile>
+                        <v-btn icon tile @click="toggleComments(item.id, question.id, i, g)">
                           <v-icon>mdi-arrow-down</v-icon>
                         </v-btn>
                       </div>
+
+                      <v-list-item-group
+                        v-if="listItems[i].data[g].showComments"
+                        multiple
+                        style="width: 100%;"
+                      >
+                        <v-list-item
+                          v-for="(comment, n) in listItems[i].data[g].comments"
+                          :key="n"
+                          class="white mt-2 mb-2"
+                          :ripple="false"
+                        >
+                          <v-list-item-content
+                            v-if="n != (listItems[i].data[g].comments.length + 1)"
+                          >
+                            <v-list-item-title
+                              class="indigo--text font-weight-bold"
+                            >{{comment.authorName}}</v-list-item-title>
+                            {{comment.description}}
+                            <div class="d-flex align-center justify-end" style="width: 100%">
+                              <v-btn icon tile>
+                                <v-icon>mdi-comment-remove-outline</v-icon>
+                              </v-btn>
+                              <v-btn icon tile>
+                                <v-icon>mdi-account-alert</v-icon>
+                              </v-btn>
+                            </div>
+                          </v-list-item-content>
+                        </v-list-item>
+                        <v-list-item v-if="listItems[i].data[g].comments.length > 0">
+                          <v-list-item-title
+                            class="indigo--text font-weight-bold text-left"
+                          >טען עוד...</v-list-item-title>
+                        </v-list-item>
+                      </v-list-item-group>
                     </v-list-item-action>
                   </v-list-item-content>
+                </v-list-item>
+                <v-list-item v-if="listItems[i].data.length > 0">
+                  <v-list-item-title class="indigo--text font-weight-bold text-center">טען עוד...</v-list-item-title>
                 </v-list-item>
               </v-list-item-group>
             </v-list>
@@ -68,7 +108,12 @@ export default {
     const { db } = require("../../services/firebase");
     const categories = await db.collection("categories").get();
     const categoriesMapped = categories.docs.map(category => {
-      return { id: category.id, name: category.data().name };
+      const categoryData = category.data();
+      return {
+        id: category.id,
+        name: categoryData.name,
+        color: categoryData.color
+      };
     });
 
     const listItems = categoriesMapped.map(item => {
@@ -92,6 +137,9 @@ export default {
   computed: {
     listItemIndex(id) {
       return this.listItems.map(item => item.id).indexOf(id);
+    },
+    colorParsed(color) {
+      return `rgba(${color[0]}, ${color[1]}, ${color[2]}, 0.9)`;
     }
   },
   methods: {
@@ -105,20 +153,52 @@ export default {
       const data = categoryQuestions.docs.map(question => {
         return {
           id: question.id,
-          ...question.data()
+          ...question.data(),
+          comments: [],
+          showComments: false
         };
       });
 
       this.$set(this.listItems[index], "show", true);
       this.$set(this.listItems[index], "data", data);
-
-      //   console.log(id);
-      //   console.log(this.listItems);
-      //   console.log(this.listItems[index]);
     },
     hideQuestions(index) {
       this.$set(this.listItems[index], "show", false);
       this.$set(this.listItems[index], "data", []);
+    },
+    async toggleComments(categoryId, questionId, categoryIndex, questionIndex) {
+      this.$set(
+        this.listItems[categoryIndex].data[questionIndex],
+        "showComments",
+        !this.listItems[categoryIndex].data[questionIndex].showComments
+      );
+
+      if (this.listItems[categoryIndex].data[questionIndex].showComments) {
+        const categoryComments = await this.db
+          .collection("categories")
+          .doc(categoryId)
+          .collection("categories_questions")
+          .doc(questionId)
+          .collection("question_comments")
+          .get();
+
+        const data = categoryComments.docs.map(comment => {
+          return {
+            id: comment.id,
+            ...comment.data()
+          };
+        });
+
+        this.$set(
+          this.listItems[categoryIndex].data[questionIndex],
+          "comments",
+          data
+        );
+
+        console.log(data);
+        console.log(this.listItems[categoryIndex].data[questionIndex]);
+        console.log(this.listItems);
+      }
     }
   }
 };
